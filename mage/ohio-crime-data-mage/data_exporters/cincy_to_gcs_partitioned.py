33abc9037c956a39afad772a05bec7df9fa5c4fb
Peter Dotterweich <58152012+PeterD5353@users.noreply.github.com>
2024-04-04 22:09:39 +0000

    file folder structure

---
+++ mage/ohio-crime-data-mage/data_exporters/cincy_to_gcs_partitioned.py
@@ -0,0 +1,34 @@
+import pyarrow as pa
+import pyarrow.parquet as pq
+import os
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/src/ohio-crime-data-project-3c5c73fae390.json"
+
+bucket_name = os.environ.get("BUCKET_NAME")
+project_id = os.environ.get("PROJECT_ID")
+
+table_name = "cincinnati_crime_data"
+
+root_path = f"{bucket_name}/{table_name}"
+
+
+@data_exporter
+def export_data(data, *args, **kwargs):
+    table = pa.Table.from_pandas(data)
+
+    gcs = pa.fs.GcsFileSystem()
+
+    pq.write_to_dataset(
+        table,
+        root_path = root_path,
+        partition_cols = ['REPORTED_DATE'],
+        filesystem = gcs
+    )
+
+
+
+
+
